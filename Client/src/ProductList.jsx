2562f0ce10be73a4b190f39cff52bf5059c95b8a
Jean Taboada Peralta <jrtaboadap@gmail.com>
2025-01-03 19:20:19 -0500

    Creamos una lista de productos renderizando uno por uno, tambien agregamos botones para poder cargar las diferentes categorias, utlizamos Axios para consumir la API

--- Client/src/ProductList.jsx
+++ Client/src/ProductList.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 export const ProductList = () => {
+
+const [products, setproducts] = useState([])
+
+const [category, setcategory] = useState("men's clothing")
+
+useEffect(() => {
+  console.log("se activo el UseEffect")
+  const fecthProducts = async()=>{
+  try {
+      const response = await axios.get(`https://fakestoreapi.com/products/category/${category}`)
+      setproducts(response.data)
+      console.log(response.data)
+    
+  } catch (error) {
+    console.log(error) 
+  }
+
+  }
+  fecthProducts()
+
+}, [category])
+
+
   return (
     <div>
-      <h1>UTILIZANDO GITHUB</h1>
+      <button onClick={()=>{setcategory('electronics')}}>Electronics</button>
+      <button onClick={()=>{setcategory('jewelery')}}>Jewelery</button>
+      <button onClick={()=>{setcategory("women's clothing")}}>women's clothing</button>
+      {
+       products.map((producto)=>(
+       <div key={producto.id}>
+        <h2>{producto.title}</h2>
+        <img src={producto.image} alt="" />
+        <h2>{producto.price}</h2>
+        </div>
+       ))
+      }
     </div>
   )
 }
